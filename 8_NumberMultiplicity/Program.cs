b51c89573775c4e86978a0a4f9a1e897f5e77004
Maksim Dorokhin <max.dorokhin@gmail.com>
2022-04-26 16:04:06 +0300

    Added task 8: Number multiplicity

---
+++ 8_NumberMultiplicity/Program.cs
@@ -0,0 +1,13 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+Console.Write("Введите проверяемое число: ");
+int numChk = int.Parse(Console.ReadLine());
+Console.Write("Введите число, кратно которому должно быть проверяемое: ");
+int numMult = int.Parse(Console.ReadLine());
+if ((numChk % numMult) == 0)
+{
+    Console.WriteLine($"Число {numChk} кратно числу {numMult}!");
+}
+else
+{
+     Console.WriteLine($"Число {numChk} не кратно числу {numMult}, остаток равен {numChk % numMult}"!);
+}
