f3a4d1e59d1b7e777dbb37c236b43dcb318d2453
Maksim Dorokhin <max.dorokhin@gmail.com>
2022-04-26 13:57:27 +0300

    Added task 1: compare a with square b

---
+++ 1_CompareWithSquare/Program.cs
@@ -0,0 +1,13 @@
+﻿//По двум заданным числам проверять является ли первое квадратом второго
+Console.Write("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+if (a == b * b)
+{
+    Console.WriteLine("Число a является квадратом числа b!");
+}
+else
+{
+    Console.WriteLine("Число a не является квадратом числа b!");
+}
