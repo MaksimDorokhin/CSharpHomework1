f4a43bee59397c5299c340f09b45f32d569c4f3a
Maksim Dorokhin <max.dorokhin@gmail.com>
2022-04-26 15:52:08 +0300

    Added task 7: Delete 2nd digit of 3-digit number

---
+++ 7_Delete2ndDigit/Program.cs
@@ -0,0 +1,13 @@
+﻿// Удалить вторую цифру трёхзначного числа
+Console.Write("Введите 3-х значное число: ");
+int n = int.Parse(Console.ReadLine());
+if (n > 99 && n < 1000)
+{
+    int dig1 = n / 100;
+    int dig3 = n % 10;
+    Console.WriteLine($"Число после удаления второй цифры: {dig1}{dig3}");
+}
+else
+{
+    Console.WriteLine("Введено не 3-х значное число!");
+}
