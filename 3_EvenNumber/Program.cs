fe5db55a016c6536421b2e4860d875f51878a8a7
Maksim Dorokhin <max.dorokhin@gmail.com>
2022-04-26 14:34:56 +0300

    Added task 3: Even Number

---
+++ 3_EvenNumber/Program.cs
@@ -0,0 +1,11 @@
+﻿//Выяснить является ли число чётным
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+if (num % 2 == 0)
+{
+    Console.WriteLine("Введенное число - четное!");
+}
+else
+{
+    Console.WriteLine("Введенное число - нечетное!");
+}
