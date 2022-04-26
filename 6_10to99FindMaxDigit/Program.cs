9c24a75237f8a99727db66f54ac736649606e25f
Maksim Dorokhin <max.dorokhin@gmail.com>
2022-04-26 15:44:40 +0300

    Added task 6: From 10 to 99 max digit

---
+++ 6_10to99FindMaxDigit/Program.cs
@@ -0,0 +1,21 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+Console.Write("Введите число из отрезка [10, 99]: ");
+int num = int.Parse(Console.ReadLine());
+if (num < 10 || num > 99)
+{
+    Console.WriteLine("Введен неверный диапазон!");
+}
+else
+{
+    Console.Write("Наибольшая цифра числа: ");
+    int dig1 = num / 10;
+    int dig2 = num % 10;
+    if (dig1 >= dig2)
+    {
+        Console.WriteLine(dig1);
+    }
+    else
+    {
+        Console.WriteLine(dig2);
+    }
+}
