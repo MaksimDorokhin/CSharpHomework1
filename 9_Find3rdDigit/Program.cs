abd14a738fb6664fe81fdd4116df12c842aa3395
Maksim Dorokhin <max.dorokhin@gmail.com>
2022-05-11 23:30:28 +0300

    Added conditions for negative numbers in task 9

--- 9_Find3rdDigit/Program.cs
+++ 9_Find3rdDigit/Program.cs
@@ -1,18 +1,20 @@
 ﻿// Найти третью цифру числа или сообщить, что её нет
 Console.Write("Введите число: ");
 int num = int.Parse(Console.ReadLine());
-if (num > 99)
+if (num > 99 && num < 1000 || num > -1000 && num < -99)
 {
-    if (num < 1000)
-    {
-        Console.WriteLine($"3-я цифра введенного числа равна {num % 10}!");
-    }
-    else
-    {
-        int count = 0;
-        int num2 = num;
-        int dig3 = num;
-        while (num2 > 0) //Считаем количество цифр введенного числа
+    Console.WriteLine($"3-я цифра введенного числа равна {Math.Abs(num % 10)}!");
+}
+else if (num >= 0 && num <= 99 || num < 0 && num >= -99)
+{
+     Console.WriteLine("У введенного числа нет 3-ей цифры!");
+}
+else
+{
+    int count = 0;
+    int num2 = Math.Abs(num);
+    int dig3 = Math.Abs(num);
+    while (num2 > 0) //Считаем количество цифр введенного числа
         {
             num2 = num2 / 10;
             count++;
@@ -22,11 +24,5 @@ if (num > 99)
             dig3 = dig3 / 10;
             count = count - 1;
         }
-        
-         Console.WriteLine($"3-я цифра введенного числа равна {dig3 % 10}");
-    }
-}
-else
-{
-     Console.WriteLine("У введенного числа нет 3-ей цифры!");
+    Console.WriteLine($"3-я цифра введенного числа равна {dig3 % 10}");
 }
